91fcdb8e7283f147f9503052b0af4c6a80fb937a
Mark J Menger <mark.j.menger@gmail.com>
2021-07-11 06:13:48 +0000

    outputs for inspec bigip-ready profile

--- test/fixtures/azure/outputs.tf
+++ test/fixtures/azure/outputs.tf
@@ -19,3 +19,31 @@ output "key_name" {
     value = var.privatekeyfile
 }
 
+output bigip_address {
+  value = azurerm_public_ip.management_public_ip[0].ip_address
+}
+
+output bigip_port {
+  value = 443
+}
+
+output user {
+  value = "admin"
+}
+
+output password {
+  value     = random_password.password.result
+  sensitive = true
+}
+
+output do_version {
+  value = "1.2.3"
+}
+
+output as3_version {
+  value = "1.3.4"
+}
+
+output ts_version {
+  value = "1.2.3"
+}
