c94218602d3ac4063b2bcad28e724d2b717bea22
Mark J Menger <mark.j.menger@gmail.com>
2021-07-13 02:40:47 +0000

    fix unnecessary use of quotes

--- test/fixtures/azure/postbuildconfig.tf
+++ test/fixtures/azure/postbuildconfig.tf
@@ -3,7 +3,7 @@ module "postbuild-config-do" {
   bigip_user       = "admin"
   bigip_password   = random_password.password.result
   bigip_address    = azurerm_public_ip.management_public_ip[0].ip_address
-  bigip_do_payload = "${file("${path.module}/../../assets/do.json")}"
+  bigip_do_payload = file("${path.module}/../../assets/do.json")
   depends_on = [
     azurerm_virtual_machine.f5bigip
   ]
