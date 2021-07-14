4b3014dbb0c022ce3cb1176a3746804f72b69a3e
Mark J Menger <mark.j.menger@gmail.com>
2021-07-14 20:22:15 +0000

    change postbuild dependencies

--- test/fixtures/azure/postbuildconfig.tf
+++ test/fixtures/azure/postbuildconfig.tf
@@ -5,6 +5,6 @@ module "postbuild-config-do" {
   bigip_address    = azurerm_public_ip.management_public_ip[0].ip_address
   bigip_do_payload = templatefile("${path.module}/../../assets/do.json", { nameserver = var.nameserver })
   depends_on = [
-    azurerm_virtual_machine.f5bigip
+    azurerm_virtual_machine_extension.run_startup_cmd
   ]
 }
