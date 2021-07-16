4327032272961341e481080e1341a2a94bb6ecf0
Mark J Menger <mark.j.menger@gmail.com>
2021-07-16 15:41:38 +0000

    copy from terraform-gcp-bigip-module example

---
+++ test/fixtures/gcp/bigip.tf
@@ -0,0 +1,12 @@
+module bigip {
+  count               = var.instance_count
+  source              = "git::git@github.com:f5devcentral/terraform-gcp-bigip-module.git?ref=v0.9.1"
+  prefix              = format("%s-3nic", var.prefix)
+  project_id          = var.project_id
+  zone                = var.zone
+  image               = var.image
+  service_account     = var.service_account
+  mgmt_subnet_ids     = [{ "subnet_id" = google_compute_subnetwork.mgmt_subnetwork.id, "public_ip" = true, "private_ip_primary" = "" }]
+  external_subnet_ids = [{ "subnet_id" = google_compute_subnetwork.external_subnetwork.id, "public_ip" = true, "private_ip_primary" = "", "private_ip_secondary" = "" }]
+  internal_subnet_ids = [{ "subnet_id" = google_compute_subnetwork.internal_subnetwork.id, "public_ip" = false, "private_ip_primary" = "", "private_ip_secondary" = "" }]
+}
