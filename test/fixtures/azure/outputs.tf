9fc437a4e0dc49ee5abd52273e068153263be51a
Mark J Menger <mark.j.menger@gmail.com>
2021-07-22 19:52:39 +0000

    remove unnecessary output from Azure fixture

--- test/fixtures/azure/outputs.tf
+++ test/fixtures/azure/outputs.tf
@@ -1,11 +1,3 @@
-output "config" {
-    value = data.azurerm_client_config.current
-}
-
-# output "bigip_mgmt_public_ips" {
-#     value = azurerm_public_ip.management_public_ip[*].ip_address
-# }
-
 output "bigip_mgmt_port" {
     value = "443"
 }
