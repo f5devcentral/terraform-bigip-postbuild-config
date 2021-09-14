5a0e5253262deb88909f73d31be27e44231cec0d
Mark J Menger <mark.j.menger@gmail.com>
2021-09-14 18:12:46 +0000

    remove static selfs and add routes for GRE traffic

--- test/fixtures/aws_tunnel/postbuildconfig.tf
+++ test/fixtures/aws_tunnel/postbuildconfig.tf
@@ -6,9 +6,13 @@ module "postbuild-config-do" {
   bigip_address    = module.bigip[count.index].mgmtPublicIP[0]
   bigip_do_payload = templatefile("${path.module}/../../assets/dotunnel.json",
       { 
-        nameserver = var.nameserver, 
-        tunnel_local_address = module.bigip[count.index == 0 ? 0 : 1].private_addresses.internal_private.private_ip[0],
-        tunnel_remote_address = module.bigip[count.index == 0 ? 1 : 0].private_addresses.internal_private.private_ip[0]
+        nameserver               = var.nameserver,
+        internal_self            = module.bigip[count.index].private_addresses.internal_private.private_ip[0],
+        internal_remote_self     = module.bigip[count.index == 0 ? 1 : 0].private_addresses.internal_private.private_ip[0],
+        external_self            = module.bigip[count.index].private_addresses.public_private.private_ip[0],
+        tunnel_overlay_address   = cidrhost("192.168.100.0/16",10 + count.index),
+        default_gateway_address  = cidrhost(cidrsubnet("${module.bigip[count.index].private_addresses.public_private.private_ip[0]}/24",0,0),1)
+        internal_gateway_address = cidrhost(cidrsubnet("${module.bigip[count.index].private_addresses.internal_private.private_ip[0]}/24",0,0),1)
       }
   )
   depends_on = [
