cd2e309deda28b8d481367d96bbb1b3c5b7fa614
Mark J Menger <mark.j.menger@gmail.com>
2021-11-02 19:17:59 +0000

    update to gcp module 1.0.0 and DO 1.25.0 for GCP fixture

--- test/fixtures/gcp/bigip.tf
+++ test/fixtures/gcp/bigip.tf
@@ -1,6 +1,6 @@
 module bigip {
   count               = var.instance_count
-  source              = "git::git@github.com:f5devcentral/terraform-gcp-bigip-module.git?ref=v0.9.3"
+  source              = "git::git@github.com:F5Networks/terraform-gcp-bigip-module.git?ref=v1.0.0"
   prefix              = format("%s-3nic", var.prefix)
   project_id          = var.project_id
   zone                = var.zone
@@ -10,6 +10,6 @@ module bigip {
   mgmt_subnet_ids     = [{ "subnet_id" = google_compute_subnetwork.mgmt_subnetwork.id, "public_ip" = true, "private_ip_primary" = "" }]
   external_subnet_ids = [{ "subnet_id" = google_compute_subnetwork.external_subnetwork.id, "public_ip" = true, "private_ip_primary" = "", "private_ip_secondary" = "" }]
   internal_subnet_ids = [{ "subnet_id" = google_compute_subnetwork.internal_subnetwork.id, "public_ip" = false, "private_ip_primary" = "", "private_ip_secondary" = "" }]
-  DO_URL              = "https://github.com/F5Networks/f5-declarative-onboarding/releases/download/v1.22.0/f5-declarative-onboarding-1.22.0-3.noarch.rpm"
-  custom_user_data = ""
+  DO_URL              = "https://github.com/F5Networks/f5-declarative-onboarding/releases/download/v1.25.0/f5-declarative-onboarding-1.25.0-7.noarch.rpm"
+  custom_user_data    = null
 }
