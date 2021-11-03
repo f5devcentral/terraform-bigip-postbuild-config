6bfbc4757be2da146af07f71c0e834d6d227fd48
Mark J Menger <mark.j.menger@gmail.com>
2021-11-03 21:52:11 +0000

    update variable descriptions for better docs

--- variables.tf
+++ variables.tf
@@ -2,42 +2,42 @@ variable "bigip_user" {
     description = "account to use with the BIG-IP"
 }
 variable "bigip_password" {
-    description = "password for BIG-IP account"
+    description = "password of `bigip_user` account"
 }
 variable "bigip_address" {
-    description = "FQDN or ip address for BIG-IP"
+    description = "FQDN or ip address of the BIG-IP"
 }
 variable "bigip_atc_payload" {
     description = "BIG-IP Automation Toolchain JSON payload - Declarative Onboarding (DO), Application Services (AS3), or other as appropriate"
 }
 
 variable "bigip_atc_endpoint" {
-    description = "atc endpoint"
+    description = "ATC endpoint to receive POSTs or GETs. The submodules (`//do` `//as3`) have these values defined."
 }
 variable "bigip_atc_status_endpoint" {
-  description = "atc endpoint for service status"
+  description = "ATC endpoint for checking the status of the service. The submodules (`//do` `//as3`) have these values defined."
 }
 variable "initial_wait" {
-  description = "time to wait before probing endpoint"
+  description = "time to wait before polling endpoint"
   default     = 30
 }
 variable "poll_interval" {
-  description = "time betweeen polling events"
+  description = "time between polling events"
   default     = 40
 }
 variable "retry_limit" {
-  description = "maximum number of attempts before exiting"
+  description = "maximum number of attempts before exiting with an error"
   default     = 10
 }
 variable "wait_for_completion" {
   type        = bool
-  description = "in case of 202 response poll for 200 before completion"
+  description = "if `true`, the module polls the service endpoint for a 200 response before exiting. if `false`, the module exits immeidately in case of and 2xx response."
   default     = true
 }
 
 variable "trigger_on_payload" {
   type        = bool
-  description = "resend the payload if the payload content has changed since the last apply"
+  description = "if `true`, the payload is sent to the endpoint during any `terraform apply` if the payload content has changed. if `false`, the payload is sent to the endpoint only during the first `terraform apply`"
   default     = true
 }
 
@@ -48,11 +48,11 @@ variable "shellscript" {
 }
 variable "additional_file_content" {
   type        = string
-  description = "additional "
+  description = "content for an additional file to place on the BIG-IP"
   default     = ""
 }
 variable "additional_file_destination" {
   type        = string
-  description = "additional file destination"
+  description = "destination for an additional file to place on the BIG-IP. used in tandem with additional_file_content"
   default     = ""
 }
