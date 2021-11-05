5c6ecdc994fe1b545d1a9b6f32877689f50cdafd
Mark J Menger <mark.j.menger@gmail.com>
2021-11-05 15:28:49 +0000

    clarify variable types

--- variables.tf
+++ variables.tf
@@ -1,33 +1,42 @@
 variable "bigip_user" {
     description = "account to use with the BIG-IP"
+    type        = string
 }
 variable "bigip_password" {
     description = "password of `bigip_user` account"
+    type        = string
 }
 variable "bigip_address" {
     description = "FQDN or ip address of the BIG-IP"
+    type        = string
 }
 variable "bigip_atc_payload" {
     description = "BIG-IP Automation Toolchain JSON payload - Declarative Onboarding (DO), Application Services (AS3), or other as appropriate"
+    type        = string
 }
 
 variable "bigip_atc_endpoint" {
     description = "ATC endpoint to receive POSTs or GETs. The submodules (`//do` `//as3`) have these values defined."
+    type        = string
 }
 variable "bigip_atc_status_endpoint" {
   description = "ATC endpoint for checking the status of the service. The submodules (`//do` `//as3`) have these values defined."
+  type        = string
 }
 variable "initial_wait" {
   description = "time to wait before polling endpoint"
   default     = 30
+  type        = number
 }
 variable "poll_interval" {
   description = "time between polling events"
   default     = 40
+  type        = number
 }
 variable "retry_limit" {
   description = "maximum number of attempts before exiting with an error"
   default     = 10
+  type        = number
 }
 variable "wait_for_completion" {
   type        = bool
