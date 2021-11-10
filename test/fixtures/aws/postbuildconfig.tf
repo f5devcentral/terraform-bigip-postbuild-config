965ebb7913afed54221085fb0a5d6c73311126e0
Mark J Menger <mark.j.menger@gmail.com>
2021-11-10 19:26:30 +0000

    AWS tagging

--- test/fixtures/aws/postbuildconfig.tf
+++ test/fixtures/aws/postbuildconfig.tf
@@ -2,7 +2,7 @@ module "postbuild-config-do" {
   source           = "../../..//do"
   bigip_user       = "admin"
   bigip_password   = random_string.password.result
-  bigip_address    = module.bigip[0].mgmtPublicIP[0]
+  bigip_address    = module.bigip[0].mgmtPublicIP
   bigip_do_payload = templatefile("${path.module}/../../assets/do.json",
   { 
     nameserver              = var.nameserver
@@ -25,7 +25,7 @@ module "postbuild-config-sh" {
   source           = "../../..//sh"
   bigip_user       = "admin"
   bigip_password   = random_string.password.result
-  bigip_address    = module.bigip[0].mgmtPublicIP[0]
+  bigip_address    = module.bigip[0].mgmtPublicIP
   bigip_shell_payload = templatefile("${path.module}/../../assets/postbuild.sh",{ maxclients = 20, mergeinterval = 10 })
   depends_on = [
     module.postbuild-config-do
