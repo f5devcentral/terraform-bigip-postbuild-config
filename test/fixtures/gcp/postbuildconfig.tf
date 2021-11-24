056c9bfbf338e326ff1e0100e67c76dcb6ed6458
Mark J Menger <mark.j.menger@gmail.com>
2021-11-24 21:47:49 +0000

    extend gcp fixture to include shell script

--- test/fixtures/gcp/postbuildconfig.tf
+++ test/fixtures/gcp/postbuildconfig.tf
@@ -19,3 +19,14 @@ module "postbuild-config-do" {
     module.bigip
   ]
 }
+
+module "postbuild-config-sh" {
+  source           = "../../..//sh"
+  bigip_user       = module.bigip[0].f5_username
+  bigip_password   = module.bigip[0].bigip_password
+  bigip_address    = module.bigip[0].mgmtPublicIP
+  bigip_shell_payload = templatefile("${path.module}/../../assets/postbuild.sh",{ maxclients = 20, mergeinterval = 10 })
+  depends_on = [
+    module.postbuild-config-do
+  ]
+}
